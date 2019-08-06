f87c67b56d115a5ca7b971aa210b38a746ccb2fc
Dani <arturo_lpc@hotmail.com>
2019-08-05 22:33:29 -0500

    Add splash screen. Big thanks to Anhar Ismail for the logo design.

---
+++ app/src/main/java/com/apps/dbm/traveldbm/activities/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.apps.dbm.traveldbm.activities;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.apps.dbm.traveldbm.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private static final int SPLASH_TIME_OUT = 8000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
+    }
+}
